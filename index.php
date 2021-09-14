fbb34b3283aceec9e0d41ca2fe032497091f6ea2
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2021-09-14 01:53:24 +0100

    php title amend

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ a {
 
 
 <section class="index-intro">
-    <h1>Vanilla pphp sign-up login project</h1>
+    <h1>Vanilla php sign-up login project</h1>
     <p>A vanilla php MySQL signup/ Login system that exercises my ability to implement hashed passwords, prepare
         statements and other forms of form validation to ensure that the data is secure, predictable and immune to SQL
         injection.
